2e4e7358dae07725268ad92e445bd83c60948651
aszlig <aszlig@nix.build>
2018-05-05 03:50:54 +0200

    habitica.nix: Add migration runner
    This is going to be used in the systemd service that's going to be
    started between the database and Habitica itself.
    
    Basically we're using the same migration runner from upstream but
    instead of starting a full server, we just require the relevant files
    needed for the migrations.
    
    The actual migration files are read from migrations.nix and those are
    numbered with an index of 1, because the state in Habitica 4.38.0 is
    schema version 0 and we want to upgrade _to_ version 1, so beginning
    with 1 is IMHO more sensible.
    
    For transpiling the migration files we use a custom gulp file, so we
    don't need to add babel-cli to our node dependencies.
    
    Note that the reason for using a list of attribute sets for
    migrations.nix instead of a list of strings is because we're going to
    directly add test scripts to the attribute set so that the tests and the
    actual migrations are close together.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- habitica.nix
+++ habitica.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, stdenv, makeWrapper, jq, nodejs-8_x
+{ lib, pkgs, stdenv, makeWrapper, writeText, jq, nodejs-8_x
 
 , habiticaConfig ? {
     NODE_ENV = "production";
@@ -89,6 +89,93 @@ in rec {
 
     installPhase = ''
       cp -rdT dist-client "$out"
+    '';
+  };
+
+  migrator = mkCommonBuild {
+    name = "migrator";
+
+    inherit (server) runtimeNodePath;
+
+    nativeBuildInputs = [ makeWrapper ];
+
+    preBuild = ''
+      sed -i -e '
+        /babel-register/d
+        /require.\+website\/server\/server/c \
+          require("../website/server/libs/setupMongoose"); \
+          require("../website/server/models/challenge"); \
+          require("../website/server/models/group"); \
+          require("../website/server/models/user");
+      ' migrations/migration-runner.js
+
+      find migrations -type f -name '*.js' -exec sed -i -e \
+        's!\(\.\./\)\+website/server!${server}/libexec/habitica/server!' {} +
+    '';
+
+    buildProg = "gulp --cwd . -f ${writeText "build-migrations.js" ''
+      const gulp = require('gulp');
+      const babel = require('gulp-babel');
+
+      gulp.task('build:migrations', () => {
+        return gulp.src('migrations/**/*.js')
+          .pipe(babel())
+          .pipe(gulp.dest('_migrations_transpiled/'));
+      });
+    ''}";
+
+    buildTarget = "build:migrations";
+
+    postBuild = ''
+      # Note that we don't use -p or anything like that, because we want this
+      # to fail if the directory already exists.
+      mkdir _migrations
+    '' + lib.concatStrings (lib.imap1 (num: { file, ... }: ''
+      migPath=${lib.escapeShellArg "_migrations_transpiled/${file}"}
+      migFile="$(basename "$migPath")"
+      migDir="_migrations/${toString num}"
+      migOutDir="$out/share/habitica/migrations/${toString num}"
+      migRunner="$migDir/runner.js"
+
+      mkdir "$migDir"
+
+      substitute _migrations_transpiled/migration-runner.js "$migRunner" \
+        --subst-var-by migrationScript "$migOutDir/$migFile"
+
+      # Move the actual migration script so that we get a build failure if we
+      # have duplicate migrations.
+      mv "$migPath" "$migDir/$migFile"
+
+      makeWrapper \
+        ${lib.escapeShellArg "${nodejs-8_x}/bin/node"} "$migDir/run.sh" \
+        --add-flags "$migOutDir/runner.js" \
+        --set NODE_ENV production \
+        --set NODE_PATH "$runtimeNodePath" \
+        --run "cd '${server}/libexec/habitica'" \
+        --run ${let
+          desc = "Running migration ${toString num} (${file})";
+        in lib.escapeShellArg "echo ${lib.escapeShellArg desc} >&2"}
+    '') (import ./migrations.nix));
+
+    installPhase = ''
+      mkdir -p "$out/bin" "$out/share/habitica"
+
+      mv _migrations "$out/share/habitica/migrations"
+
+      cat > "$out/bin/migrate" <<EOF
+      #!${stdenv.shell} -e
+      usage() {
+        echo "Usage \$0 MIGRATION_NUMBER" >&2
+        echo "Run the specified database migration number on Habitica" >&2
+      }
+
+      if [ -z "\$1" ]; then
+        usage; exit 1
+      else
+        exec "$out/share/habitica/migrations/\$1/run.sh" "\$@"
+      fi
+      EOF
+      chmod +x "$out/bin/migrate"
     '';
   };
 
---
+++ migrations.nix
@@ -0,0 +1,6 @@
+# This lists all of the upgrades required to transform the database to the
+# latest version. Of course order is important, because the upgrades are
+# applied in sequence.
+[
+  { file = "groups/migrate-chat.js"; }
+]
