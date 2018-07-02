078b10a9bcc9812a385d070f46633947e5cb9464
aszlig <aszlig@nix.build>
2018-07-02 07:49:07 +0200

    Add a helper to remove killed files
    With filesToKill being exposed in source.nix, we now have a small helper
    utility which can quickly remove all the files from the source tree that
    we do not want to have.
    
    Running rm -r on these files however is a bit scary, so we might want to
    switch to a better solution in the future.
    
    Signed-off-by: aszlig <aszlig@nix.build>

---
+++ kill-files.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -p bash nix jq -i bash
+
+set -e
+
+if ! grep -q 'name.*habitica' package.json 2> /dev/null; then
+  echo "You need to run this from within a Habitica git clone." >&2
+  exit 1
+fi
+
+getKillFiles() {
+  ( cd "$(dirname "$0")"
+    nix-instantiate --eval --json -E '
+      ((import <nixpkgs> {}).callPackages ./habitica.nix {}).source.filesToKill
+    '
+  )
+}
+
+getKillFiles | jq -r '.[] | @sh "rm -r \(.)"' | $SHELL
