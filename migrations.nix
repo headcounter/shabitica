3722218ceb6218c165e181f9cbdec37dc525394d
aszlig <aszlig@nix.build>
2018-10-10 02:45:34 +0200

    migrations: Replace assertEquals by assertEqual
    The use of the assertEquals alias is deprecated since Python 3.2[1], so
    let's use assertEqual instead.
    
    [1]: https://docs.python.org/dev/whatsnew/3.2.html#unittest
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- migrations.nix
+++ migrations.nix
@@ -6,7 +6,7 @@
     testScript = ''
       spec = getuser('foo')
       party = spec.api.groups[spec.partyId].get()
-      assertEquals(party['chat'][0]['text'], "Hello World!")
+      assertEqual(party['chat'][0]['text'], "Hello World!")
     '';
   }
   { file = "tasks/habits-one-history-entry-per-day-users.js";
@@ -15,7 +15,7 @@
       habit = spec.api.tasks[spec.habitId].get()
 
       assertIn('history', habit)
-      assertEquals(len(habit['history']), 2)
+      assertEqual(len(habit['history']), 2)
 
       for hist in habit['history']:
         assertIn('date', hist)
@@ -23,11 +23,11 @@
         assertIn('scoredUp', hist)
         assertIn('scoredDown', hist)
 
-      assertEquals(habit['history'][0]['scoredUp'], 3)
-      assertEquals(habit['history'][0]['scoredDown'], 1)
+      assertEqual(habit['history'][0]['scoredUp'], 3)
+      assertEqual(habit['history'][0]['scoredDown'], 1)
 
-      assertEquals(habit['history'][1]['scoredUp'], 1)
-      assertEquals(habit['history'][1]['scoredDown'], 0)
+      assertEqual(habit['history'][1]['scoredUp'], 1)
+      assertEqual(habit['history'][1]['scoredDown'], 0)
     '';
   }
   { file = "tasks/habits-one-history-entry-per-day-challenges.js";
@@ -35,7 +35,7 @@
       spec = getuser('foo')
       challenge_tasks = spec.api.tasks.challenge[spec.challengeId].get()
 
-      assertEquals(len(challenge_tasks), 1)
+      assertEqual(len(challenge_tasks), 1)
 
       challenge_habit = challenge_tasks[0]
       print(challenge_habit['history'])
@@ -46,11 +46,11 @@
         assertIn('scoredUp', hist)
         assertIn('scoredDown', hist)
 
-      assertEquals(challenge_habit['history'][0]['scoredUp'], 0)
-      assertEquals(challenge_habit['history'][0]['scoredDown'], 1)
+      assertEqual(challenge_habit['history'][0]['scoredUp'], 0)
+      assertEqual(challenge_habit['history'][0]['scoredDown'], 1)
 
-      assertEquals(challenge_habit['history'][1]['scoredUp'], 1)
-      assertEquals(challenge_habit['history'][1]['scoredDown'], 0)
+      assertEqual(challenge_habit['history'][1]['scoredUp'], 1)
+      assertEqual(challenge_habit['history'][1]['scoredDown'], 0)
     '';
   }
 ]
