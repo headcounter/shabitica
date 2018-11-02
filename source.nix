3fe2fc905f0258f548f316d25d92c63b34229638
aszlig <aszlig@nix.build>
2018-11-02 09:22:15 +0100

    Update to version 4.68.1
    Another very small update, which just ends the Habitoween/Fall Fest. No
    rebase needed for our patches.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- source.nix
+++ source.nix
@@ -6,14 +6,14 @@ stdenv.mkDerivation rec {
   name = "shabitica-source-${version}";
   # NOTE: If appropriate, run update-deps.py after changing this!
   #       Also, don't forget to run ./find-canaries.py after rebasing patches.
-  version = "4.68.0";
+  version = "4.68.1";
 
   src = fetchFromGitHub {
     name = "habitica-source-${version}";
     owner = "HabitRPG";
     repo = "habitica";
     rev = "v${version}";
-    sha256 = "1pkhql84bw8hdxjfphhivd5fsk1ym5y3n7xym60185fv4gf48avd";
+    sha256 = "0y42fj2xl6xl388fws74ibp78gyrsv5hzvhmj0s1n5gr1z29l3bi";
   };
 
   phases = [ "unpackPhase" "patchPhase" "checkPhase" "installPhase" ];
