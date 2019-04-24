5ae4e5001b7ca22141f609327767ea9a9dd46b10
aszlig <aszlig@nix.build>
2019-04-24 23:10:46 +0200

    Update to version 4.92.6
    Upstream changes for version 4.92.6:
    
      API:
    
        * Fix: Disallow negative and fractional purchase quantities for
               Market items like quest scrolls
        * Chore: Bailey blog post announcement
    
      Client:
    
        * Fix: Disallow negative and fractional purchase quantities for
               Market items like quest scrolls
    
    Upstream changes for version 4.92.5:
    
      API:
    
        * Fix: Remove item from tasksOrder array in group when deleting a
               Group Plan task
        * Chore: Bailey news announcement re Use Case Spotlight blog post
        * Chore: End split testing for welcome emails. Send blog link
                 variant to Web users and tips email variant to mobile users
    
      Client:
    
        * Fix: Employ challenge pagination on My Challenges page for better
               performance
        * Fix: Improve description of class-locked items
        * Fix: Various UX improvements to Group Plans workflows
    
    Upstream changes for version 4.92.4:
    
      * Fix: Restore accidentally removed contributor tier styling from
             usernames in chat
    
    A few new references of analytics were added to the tests, which I
    subsequently removed in the patches, other than that, the rebase of the
    patches only caused a few very minor conflicts.
    
    The biggest change that stands out in 4.92.5 is the UX overhaul for
    group plans, where the change actually is a squashed commit of the
    following changes:
    
      * Remove an unused isManager function from group-plans/index.vue
      * Update the task creation control on the group plans page to current
        foldout button style
      * Show/hide the task creation control based on whether the user is
        allowed to create group tasks
      * Remove raw JavaScript confirmation dialog when approving group tasks
      * Disable task dragging on the Group Plan task board
      * Reenable the "..." options button on Group Plan tasks, for quick
        access to the delete button
      * Fix an API issue where deleting Group Plan tasks did not remove the
        corresponding tasksOrder entry
      * Add a sync when the user claims or unclaims a task
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- source.nix
+++ source.nix
@@ -6,14 +6,14 @@ stdenv.mkDerivation rec {
   name = "shabitica-source-${version}";
   # NOTE: If appropriate, run update-deps.py after changing this!
   #       Also, don't forget to run ./find-canaries.py after rebasing patches.
-  version = "4.92.3";
+  version = "4.92.6";
 
   src = fetchFromGitHub {
     name = "habitica-source-${version}";
     owner = "HabitRPG";
     repo = "habitica";
     rev = "v${version}";
-    sha256 = "03c6k1dzmklzhnm6fs23i8idhi3fl9lkhg8xnk41mmh8afmp9002";
+    sha256 = "051wgxxsg3j89ci7aifa8250s6jmalm1fwalaxl1dvyl9q193lvh";
   };
 
   phases = [ "unpackPhase" "patchPhase" "checkPhase" "installPhase" ];
