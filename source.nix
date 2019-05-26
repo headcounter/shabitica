96fec4ad0dc14fcf730a7adefb0f62fb4cbab2ea
aszlig <aszlig@nix.build>
2019-05-26 05:07:06 +0200

    Update to version 4.96.1
    This is only a small bugfix release:
    
      * Fix: Replace unavailable item in Quest Shop featured items list
      * Chore: Bailey news announcement re: monthly blog features
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- source.nix
+++ source.nix
@@ -6,14 +6,14 @@ stdenv.mkDerivation rec {
   name = "shabitica-source-${version}";
   # NOTE: If appropriate, run update-deps.py after changing this!
   #       Also, don't forget to run ./find-canaries.py after rebasing patches.
-  version = "4.96.0";
+  version = "4.96.1";
 
   src = fetchFromGitHub {
     name = "habitica-source-${version}";
     owner = "HabitRPG";
     repo = "habitica";
     rev = "v${version}";
-    sha256 = "1qxvw6rpxjffyq90yl2lmpjj6va8y1wa8z374qr1ywbqqlilamgy";
+    sha256 = "1gj7yfcab2pivgm28cfdjbpckliir2kmdcqsnvxlyl08fp10gigy";
   };
 
   phases = [ "unpackPhase" "patchPhase" "checkPhase" "installPhase" ];
