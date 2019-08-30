c0781036520bf51517be5e3826818c3504d93c40
aszlig <aszlig@nix.build>
2019-08-30 04:42:08 +0200

    imageproxy: Update go-camo to version 1.1.7
    Among a few security fixes, here are the changes since version 1.1.2:
    
      Version 1.1.7:
    
        * Remove old stats flag, endpoint, and feature, in favor of the new
          prometheus endpoint. Good amount of code removal as well
        * Use a sync.Pool []byte buffer for io.CopyBuffer (instead of
          io.Copy). It should reduce some small amount of GC pressure (a bit
          less garbage)
        * Build with go 1.12.8 - which includes security fixes for HTTP/2
    
      Version 1.1.6:
    
        * Support range requests to get safari video support working
    
      Version 1.1.5:
    
        * Security fixes / SSRF:
          * Ensure non-GET/HEAD request does not send outbound request
          * Validate redirect urls the same as initial urls
        * Split out exception for missing content types
        * Prometheus compatible metrics endpoint added
        * Disable credential/userinfo (user:pass@ style) type urls by
          default
        * Added cli flag (--allow-credential-urls) to retain prior behavior
          (which allows them)
    
      Version 1.1.4:
    
        * Disable passing/generating x-forwarded-for header by default
        * Add new --enable-xfwd4 flag to enable x-forwarded-for header
          passing/generation
        * Add optional json output for stats
        * Remove gomaxprocs code, as it is no longer necessary
        * Documentation fixes (man page update, spelling, etc)
        * Build release with go-1.12
    
      Version 1.1.3:
    
        * Fix SSRF leak, where certain requests would not match defined and
          custom IP blacklists as expected
    
    I've rebased our patches and also added a new one which removes the
    prometheus dependency introduced in version 1.1.5 as we're not using the
    metrics feature.
    
    Other than that, I've trimmed down the dependencies to only use the ones
    we actually need to build the application.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- imageproxy.nix
+++ imageproxy.nix
@@ -5,20 +5,21 @@ let
 
   go-camo = pkgs.buildGoPackage rec {
     name = "go-camo-${version}";
-    version = "1.1.2git2018-08-01";
+    version = "1.1.7";
 
     goPackagePath = "github.com/cactus/go-camo";
 
     src = pkgs.fetchFromGitHub {
       owner = "cactus";
       repo = "go-camo";
-      rev = "7f62c2477c1072d7e7ef72962380ae8238a74e2c";
-      sha256 = "0mx0scykr0dwshjqcc7ssqvlgdqn5rh554pfgxwk6wzh0cxv00qq";
+      rev = "v${version}";
+      sha256 = "19fayml2138wk0lkbslwn1yg4bwcw88h09r1iwbr7xf7sjabad8k";
     };
 
     patches = [
-      ./patches/imageproxy-socket-activation.patch
-      ./patches/imageproxy-decode-session.patch
+      patches/imageproxy-socket-activation.patch
+      patches/imageproxy-decode-session.patch
+      patches/imageproxy-remove-metrics.patch
     ];
 
     extraSrcs = let
@@ -42,29 +43,13 @@ let
         rev = "v1.0.0";
         sha256 = "0x4dk5sw4ypiwd7gr5rjzp4avz0sagzs42r3dw852flh3lpvf4ir";
       }
-      { goPackagePath = "github.com/davecgh/go-spew";
-        rev = "v1.1.0";
-        sha256 = "0d4jfmak5p6lb7n2r6yvf5p1zcw0l8j74kn55ghvr7zr7b7axm6c";
-      }
       { goPackagePath = "github.com/jessevdk/go-flags";
         rev = "v1.3.0";
         sha256 = "1jk2k2l10lwrn1r3nxdvbs0yz656830j4khzirw8p4ahs7c5zz36";
       }
-      { goPackagePath = "github.com/pelletier/go-toml";
-        rev = "v1.1.0";
-        sha256 = "1y5m9pngxhsfzcnxh8ma5nsllx74wn0jr47p2n6i3inrjqxr12xh";
-      }
-      { goPackagePath = "github.com/pmezard/go-difflib";
-        rev = "v1.0.0";
-        sha256 = "0c1cn55m4rypmscgf0rrb88pn58j3ysvc2d0432dp3c6fqg6cnzw";
-      }
-      { goPackagePath = "github.com/stretchr/testify";
-        rev = "v1.2.1";
-        sha256 = "01f80s0q64pw5drfgqwwk1wfwwkvd2lhbs56lhhkff4ni83k73fd";
-      }
       { goPackagePath = "github.com/coreos/go-systemd";
-        rev = "v17";
-        sha256 = "1kzqrrzqspa5qm7kwslxl3m16lqzns23c24rv474ajzwmj3ixmx1";
+        rev = "v20";
+        sha256 = "0jf705z62hy1230ryw9z3686g31v5yc16izcjzcasbawgv5c000v";
       }
     ];
   };
