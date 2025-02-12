29e6b84c541cc05279ae72f34bc22e9ca4ec03ab
Leonard-Orlando Jonathan Menzel <leonard-orlando.jonathan.menzel@stud.th-luebeck.de>
2025-02-12 13:08:56 +0000

    nixosModules without system

--- flake.nix
+++ flake.nix
@@ -2,19 +2,16 @@
   description = "shabitica flake";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/21.05";
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosModules = builtins.mapAttrs
-      (system: pkgs: {
-        default =
-          { lib, config, ... }: {
-            imports = [
-              (import ./default.nix {
-                inherit lib config;
-                pkgs = import nixpkgs { inherit system; };
-              })
-            ];
+  outputs = { nixpkgs, ... }: {
+    nixosModules.default = { lib, config, ... }: {
+      imports = [
+        (import ./default.nix {
+          inherit lib config;
+          pkgs = import nixpkgs {
+            inherit (config.nixpkgs.localSystem) system;
           };
-      })
-      nixpkgs.legacyPackages;
+        })
+      ];
+    };
   };
 }
